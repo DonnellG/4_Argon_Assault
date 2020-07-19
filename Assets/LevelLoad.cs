9a1c738cc9f4162bac53e17abd47fcdc0c506480
dgrantham <zankokudonnell@gmail.com>
2020-07-19 16:52:07 -0400

    83_MusicBetweenScenes

---
+++ Assets/LevelLoad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    [SerializeField] float levelLoadDely = 2f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstLevel", levelLoadDely);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
