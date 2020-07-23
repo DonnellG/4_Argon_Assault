e93e478f3d3c33f76088ac54643f3586d781869e
dgrantham <zankokudonnell@gmail.com>
2020-07-23 08:22:46 -0400

    91_Rotate_Ship

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -11,6 +12,18 @@ public class Player : MonoBehaviour
     [Tooltip("In ms^-1")] [SerializeField] float yspeed = 15f;
     [Tooltip("in m")] [SerializeField] float yPosMin = 3f;
     [Tooltip("in m")] [SerializeField] float yPosMax = 3f;
+
+    [SerializeField] float xPitchMax = -5f;
+    [SerializeField] float controlPitchFactor = -30f;
+
+    [SerializeField] float yYawMax = 5f;
+
+
+    [SerializeField] float zRollControlFactorMax = -30f;
+
+    float xThrow, yThrow;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,15 +33,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * xPitchMax;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * yYawMax;
+
+        float roll = xThrow * zRollControlFactorMax;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xspeed * Time.deltaTime;
 
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xPosRange, xPosRange);
 
-        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
-
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * yspeed * Time.deltaTime;
 
         float rawYPos = transform.localPosition.y + yOffset;
